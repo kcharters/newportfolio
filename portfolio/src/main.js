f2ca62391318b08585b2ab599b13eb62aa5035b5
Kirstin Charters <Kirstin@qbt.com.au>
2021-10-28 13:44:39 +1300

    init

---
+++ portfolio/src/main.js
@@ -0,0 +1,12 @@
+/*!
+ * fullPage 2.9.7
+ * https://github.com/alvarotrigo/fullPage.js
+ * @license MIT licensed
+ *
+ * Copyright (C) 2015 alvarotrigo.com - A project by Alvaro Trigo
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+
+createApp(App).mount('#app')
